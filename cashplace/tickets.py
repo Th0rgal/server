02e84faa100b85083df436db5086d08cee770101
Th0rgal <thomas.marchand@tuta.io>
2020-08-01 21:55:05 +0200

    generate bitcoin addresses

--- cashplace/tickets.py
+++ cashplace/tickets.py
@@ -1,20 +1,27 @@
 from bit import Key, PrivateKeyTestnet
 
 
-class Ticket:
-    pass
-
-
-class BitcoinTicket(Ticket):
-    pass
-
-
 class TicketsFactory:
     def __init__(self, config):
         self.config = config
 
     def create_ticket(self, coin):
         if coin == "btc":
-            return Ticket()
+            return BitcoinTicket()
         else:
-            pass
+            return False
+
+
+class Ticket:
+    pass
+
+
+class BitcoinTicket(Ticket):
+    def __init__(self):
+        self.key = PrivateKeyTestnet()
+        self.public_address = self.key.segwit_address
+        self.wif = self.key.to_wif()
+
+    @property
+    def id(self):
+        return self.public_address
--- cashplace/web/queries.py
+++ cashplace/web/queries.py
@@ -6,13 +6,15 @@ from errors import InvalidWebInput
 class Queries:
     def __init__(self, config):
         self.config = config
+        self.factory = TicketsFactory(config)
 
     def register_routes(self, app):
         app.add_routes([web.get("/btc/new/{coin}", self.create_ticket)])
 
     async def create_ticket(self, request):
         coin = request.match_info.get("coin").lower()
-        if coin == "btc":
-            return web.json_response({"address": "0"})
+        ticket = self.factory.create_ticket(coin)
+        if ticket:
+            return web.json_response({"id": ticket.id})
         else:
             raise InvalidWebInput(f"unknown coin name: {coin}, valid names: [btc]")
